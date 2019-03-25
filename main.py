394fbbb23107a973e7f35366343acd998444de4d
Francisco Robles Castro <franrobles8@gmail.com>
2019-03-25 19:32:42 +0100

    Reestructuracion de codigo. Anadidas algunas mejoras, como parametros en el constructor de mser y correcta ampliacion de los puntos de los bboxes. Tambien se ha hecho un poco de limpieza en el codigo.

--- main.py
+++ main.py
@@ -11,18 +11,37 @@ def transformToPoints(bboxes):
     return bboxesPuntos
 
 def getFormattedPoint(oldFormatRectangle):
-    extraespace = 1.5
 
-    # ampliamos area del cuadrado (dismunuimos la x e y del primer punto y aumentamos ancho y alto)
-    oldFormatRectangle[0] = oldFormatRectangle[0]*0.99
-    oldFormatRectangle[1] = oldFormatRectangle[1]*0.99
-    oldFormatRectangle[2] = oldFormatRectangle[2]*1.4
-    oldFormatRectangle[3] = oldFormatRectangle[3]*1.4
+    # Creamos los puntos con los datos obtenidos de los bboxes en formato [x ,y , ancho, alto]
+    coord_x = oldFormatRectangle[0]
+    coord_y = oldFormatRectangle[1]
+    ancho = oldFormatRectangle[2]
+    alto = oldFormatRectangle[3]
 
-    point1 = [oldFormatRectangle[0], oldFormatRectangle[1]]
-    point2 = [oldFormatRectangle[0] + oldFormatRectangle[2], oldFormatRectangle[1]]
-    point3 = [oldFormatRectangle[0] + oldFormatRectangle[2], oldFormatRectangle[1] + oldFormatRectangle[3]]
-    point4 = [oldFormatRectangle[0], oldFormatRectangle[1] + oldFormatRectangle[3]]
+    point1 = [coord_x, coord_y]
+    point2 = [coord_x + ancho, coord_y]
+    point3 = [coord_x + ancho, coord_y + alto]
+    point4 = [coord_x, coord_y + alto]
+
+    # ampliamos area del cuadrado (disminuimos la x e y del primer punto y aumentamos ancho y alto)
+    # la coordenada y es positiva hacia abajo (al reves de como suele ser normalmente)
+    ampliacion = ancho/5 # expandimos el bbox 1/5 de su tamanio original
+
+    # punto superior izquierdo
+    point1[0] = point1[0] - ampliacion
+    point1[1] = point1[1] - ampliacion
+
+    # punto superior derecho
+    point2[0] = point2[0] + ampliacion
+    point2[1] = point2[1] - ampliacion
+
+    # punto inferior derecho
+    point3[0] = point3[0] + ampliacion
+    point3[1] = point3[1] + ampliacion
+
+    # punto inferior izquierdo
+    point4[0] = point4[0] - ampliacion
+    point4[1] = point4[1] + ampliacion
 
     return np.array([point1, point2, point3, point4], dtype=np.int32)
 
@@ -36,18 +55,24 @@ def filterSquares(bboxes):
     return bboxesPuntosFiltered
 
 
-mser = cv.MSER_create()
+# mser = cv.MSER_create()
 
+# Orden de los parametros del constructor
+#       _delta,_min_area,_max_area,
+#       _max_variation, _min_diversity,
+#       _max_evolution, _area_threshold,
+#       _min_margin, _edge_blur_size
+mser = cv.MSER_create(3, 100, 2000, 0.3, 1.0, 200, 1.01, 0.003, 0)
 
 while True:
 
-    # Cargamos la imagen en escala de grises --> 0
-    img = cv.imread('train_10_ejemplos/00001.ppm', 1)
+    # Cargamos la imagen en escala de grises
+    img = cv.imread('train_10_ejemplos/00001.ppm', cv.IMREAD_GRAYSCALE)
 
     imgCopy = img.copy()
 
-    # Guardamos las Bounding Boxes de la imagen -> (x1,y1) y (x2, y2)
-    regions, bboxes = mser.detectRegions(img)
+    # Guardamos las Bounding Boxes de la imagen -> (x1,y1) y ancho y alto
+    regions, bboxes = mser.detectRegions(imgCopy)
 
     # hulls son todos los contornos, y convexHull nos da el contorno cerrado de una region convexa
     # con reshape, cambiamos las dimensiones de la matriz sin alterar su contenido
@@ -55,24 +80,9 @@ while True:
 
     hulls = [cv.convexHull(p.reshape(-1, 1, 2)) for p in filterSquares(bboxes)]
 
-    """
-    for h in hulls:
-        print (h)
-        print ("End hull-----------------")
-    """
-
     cv.polylines(imgCopy, hulls, 1, (0, 255, 0))
-    print("Fin rect-------------------")
 
     cv.imshow('img', imgCopy)
-
-    """
-    for b in bboxes:
-        print (b)
-        print ("Fin rect-------------------")
-    """
-
-
 
     # Cerramos la ventana presionando escape
     if cv.waitKey(5) == 27:
